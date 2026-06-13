c32aa9fb745ff9936136685614c6c17403774587
Nadia Lubis <lubisputri16@gmail.com>
2026-06-13 21:53:51 +0700

    Initial Commit

---
+++ graspp/grasppApp.swift
@@ -0,0 +1,17 @@
+//
+//  grasppApp.swift
+//  graspp
+//
+//  Created by Nadia Putri Natali Lubis on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct grasppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
