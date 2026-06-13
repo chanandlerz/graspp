ce3a7377961be3bdfcf85efd09c8325cee6f6af4
Nadia Lubis <lubisputri16@gmail.com>
2026-06-13 23:16:42 +0700

    feat. add subview FavoriteProgressive with CardFavorite component

---
+++ graspp/Components/CardFavorite.swift
@@ -0,0 +1,18 @@
+//
+//  CardFavorite.swift
+//  graspp
+//
+//  Created by Nadia Putri Natali Lubis on 13/06/26.
+//
+
+import SwiftUI
+
+struct CardFavorite: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardFavorite()
+}
---
+++ graspp/SubViews/FavoritesProgressive.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesProgressive.swift
+//  graspp
+//
+//  Created by Nadia Putri Natali Lubis on 13/06/26.
+//
+
+import SwiftUI
+
+struct FavoritesProgressive: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesProgressive()
+}
