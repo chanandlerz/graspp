c414807d3cb6cdc2ad6fdbe5394197e9de83eb60
Nadia Lubis <lubisputri16@gmail.com>
2026-06-18 11:26:15 +0700

    (fix) remove preview comment

--- graspp/Screens/ArticleListScreen.swift
+++ graspp/Screens/ArticleListScreen.swift
@@ -34,20 +34,6 @@ struct ArticleListScreen: View {
     }
 }
 
-//#Preview {
-//    ScrollView {
-//        LazyVStack(spacing: 12) {
-//            ForEach(sampleArticles) { article in
-//                NavigationLink(value: article) {
-//                    CardArticleList(article: article)
-//                }
-//                .buttonStyle(.plain)
-//            }
-//        }
-//        .padding(.horizontal)
-//    }
-//    .modelContainer(previewContainer)
-//}
 
 #Preview {
     NavigationStack {
